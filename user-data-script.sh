4320ce094cbe03e2e181ce2da374b0d1fad10a6e
Andromeda0410 <rathauryashwardhan@gmail.com>
2026-06-06 13:26:18 +0530

    Update user-data-script.sh
    Fix metadata retrieval using IMDSv2 token authentication

--- user-data-script.sh
+++ user-data-script.sh
@@ -6,14 +6,17 @@ apt update -y
 # Install Apache web server
 apt install apache2 -y
 
-# Fetch EC2 Instance ID from AWS Metadata Service
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+# Get IMDSv2 token
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
+-H "X-aws-ec2-metadata-token-ttl-seconds: 21600" -s)
 
-# Create a simple webpage displaying the instance ID
+# Get EC2 Instance ID
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" \
+-s http://169.254.169.254/latest/meta-data/instance-id)
+
+# Create webpage
 echo "<h1>Instance ID: $INSTANCE_ID</h1>" > /var/www/html/index.html
 
-# Start Apache service
+# Start Apache
 systemctl start apache2
-
-# Ensure Apache starts automatically on reboot
 systemctl enable apache2
